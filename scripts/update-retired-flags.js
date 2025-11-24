48b1d838b0fd21a2ae58e5e8eb7b89501ca2f529
Pablo Camardelli <pecamardelli@gmail.com>
2025-11-24 01:47:02 -0300

    Add races

---
+++ scripts/update-retired-flags.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+// CONFIGURATION - Modify these variables
+const RACE_FILE = 'stats_monaco_1966_session_race_20251123_210500.json';
+const MIN_LAPS_THRESHOLD = 18;
+
+// File path construction
+const filePath = path.join(__dirname, '../app/data/championship/Test Drive Tour/season_02', RACE_FILE);
+
+const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+const driverNames = Object.keys(data.driver_statistics);
+console.log('Total drivers:', driverNames.length);
+console.log(`Checking for drivers with < ${MIN_LAPS_THRESHOLD} laps...\n`);
+
+let updatedCount = 0;
+driverNames.forEach(name => {
+  const driver = data.driver_statistics[name];
+  if (driver.laps_completed < MIN_LAPS_THRESHOLD && !driver.retired) {
+    console.log(`Setting retired=true for ${name}: ${driver.laps_completed} laps`);
+    driver.retired = true;
+    updatedCount++;
+  }
+});
+
+console.log(`\nUpdated ${updatedCount} drivers to retired=true`);
+
+fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+console.log('File updated successfully!');
