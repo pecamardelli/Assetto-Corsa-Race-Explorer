8e7820e38a16fd5342a72e31b32e69e7ce1dd9ed
Pablo Camardelli <pecamardelli@gmail.com>
2025-12-20 00:07:58 -0300

    Add races

--- scripts/update-retired-flags.js
+++ scripts/update-retired-flags.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const path = require("path");
 
 // CONFIGURATION - Modify these variables
-const RACE_FILE = "stats_battenbergring_session_race_20251216_004627.json";
-const MIN_LAPS_THRESHOLD = 7;
+const RACE_FILE = "stats_monaco_1966_session_race_20251219_234513.json";
+const MIN_LAPS_THRESHOLD = 20;
 
 // File path construction
 const filePath = path.join(
